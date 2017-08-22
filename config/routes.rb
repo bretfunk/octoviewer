eac5c96eeeb3ea55036c30e005eb060115d7efc6
BFunk <bret.e.funk@gmail.com>
2017-08-22 12:12:22 -0600

    created dashboard with basic github info

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/auth/github", as: "github_login"
   get "/auth/github/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destory"
+  get "/dashboard", to: "dashboard#show"
 end
